7aedb699cc4c5690c68f86f8c4b197eb9741fcd5
Mohammad Pavel Hossain <51057851+pavel99pop@users.noreply.github.com>
2024-05-23 18:04:38 +0000

    Finished chapter one

--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -7,8 +7,9 @@
 
 int main(){
     std::string str;
+    std::cout << "Enter your name: " << std::flush;
     std::cin >> str;
-    std::cout << str;
+    std::cout << "Well hello " << str << "!" << std::endl;
 
     std::cout << std::endl << std::endl;
     return (0);
