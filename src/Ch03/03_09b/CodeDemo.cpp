694959d593b533d68e4249184a6bf57ab1a2c37d
Mohammad Pavel Hossain <51057851+pavel99pop@users.noreply.github.com>
2024-05-31 17:44:36 +0000

    Finished Ch 3

--- src/Ch03/03_09b/CodeDemo.cpp
+++ src/Ch03/03_09b/CodeDemo.cpp
@@ -4,9 +4,23 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 int main(){
-    
+    std::vector<int> primes;
+    primes.push_back(16);
+    primes.push_back(68);
+    primes.push_back(47);
+    primes.push_back(15);
+    primes.push_back(39);
+
+    std::cout << "The vector has " << primes.size() << " elements." << std::endl;
+
+    std::cout << "The content of index 2 is: " << primes[2] << std::endl;
+
+    primes[2] = 69;
+    std::cout << "The content of index 2 is: " << primes[2] << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
