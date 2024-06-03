2970497ce058ed7ce4792b969e1daf8d5fb821a9
Mohammad Pavel Hossain <51057851+pavel99pop@users.noreply.github.com>
2024-06-03 18:57:43 +0000

    Finished Ch 5

--- src/Ch05/05_04/records.cpp
+++ src/Ch05/05_04/records.cpp
@@ -1,4 +1,5 @@
 #include "records.h"
+#include "iostream"
 
 Student::Student(int the_id, std::string the_name){
     id = the_id;
@@ -78,6 +79,13 @@ std::string StudentRecords::get_student_name(int sid) const{
     return students[i].get_name();
 }
 
+std::string StudentRecords::get_course_name(int cid) const{
+    int j = 0;
+    while (j < courses.size() && courses[j].get_id() != cid)
+        j++;
+    return courses[j].get_name();
+}
+
 unsigned char StudentRecords::get_course_credits(int cid) const{
     int j = 0;
     while (j < courses.size() && courses[j].get_id() != cid)
@@ -94,4 +102,18 @@ float StudentRecords::get_GPA(int sid) const{
             points += get_num_grade(grd.get_grade()) * current_credits;
         }
     return (points / credits);
+}
+
+void StudentRecords::report_card(int sid) {
+    float points = 0.0f, credits = 0.0f, GPA;
+    std::cout << "Report Card of " << get_student_name(sid) << std::endl;
+    for (const Grade& grd : grades)
+        if (grd.get_student_id() == sid){
+            unsigned char current_credits = get_course_credits(grd.get_course_id());
+            std::cout << get_course_name(grd.get_course_id()) << ": " << grd.get_grade() << std::endl;
+            credits += current_credits;
+            points += get_num_grade(grd.get_grade()) * current_credits;
+        }
+    GPA = (points / credits);
+    std::cout << "GPA: " << GPA << std::endl;
 }
